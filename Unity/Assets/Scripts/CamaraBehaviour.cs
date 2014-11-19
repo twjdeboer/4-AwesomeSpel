13972609eeab3fd122e5d0965bd3248a8fee6acb
Boyd Verdoorn <B.C.Verdoorn@student.tudelf.nl>
2014-11-19 15:20:39 +0100

    Doorzichtigheid verbeterd ---> object tussen camera en player wordt uit zichzelf gecreerd.

--- Unity/Assets/Scripts/CamaraBehaviour.cs
+++ Unity/Assets/Scripts/CamaraBehaviour.cs
@@ -4,13 +4,16 @@ using System.Collections;
 public class CamaraBehaviour : MonoBehaviour {
 
     private Vector3 offset;
+    private GameObject prefab;
 
 
 	// Use this for initialization
 	void Start () {
 
-        this.offset = transform.position;
+        this.offset = transform.position - ResourceManager.playerPosition;
         ResourceManager.cam = transform;
+        prefab = Resources.Load("Prefabs/viewLine") as GameObject;
+        CreateViewLine(prefab);
 	}
 
     void FollowPlayer()
@@ -18,6 +21,15 @@ public class CamaraBehaviour : MonoBehaviour {
         transform.position = ResourceManager.playerPosition + this.offset;
     }
 	
+    void CreateViewLine(GameObject prefab)
+    {
+        GameObject viewLine = Instantiate(prefab, this.offset * 0.5f, Quaternion.Euler(0, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z)) as GameObject;
+        viewLine.transform.parent = transform;
+        viewLine.transform.localScale = new Vector3(5, this.offset.y,this.offset.z);
+    
+    }
+
+
 	// Update is called once per frame
 	void FixedUpdate () {
 
