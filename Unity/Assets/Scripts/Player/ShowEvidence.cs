9c754aeb42395db52b9ee5ace011ab31015baf1d
Boyd Verdoorn <B.C.Verdoorn@student.tudelf.nl>
2014-12-16 12:35:24 +0100

    Beginnetje aan evidenceList weergeven.

---
+++ Unity/Assets/Scripts/Player/ShowEvidence.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+
+public class ShowEvidence : MonoBehaviour
+{
+    private List<GameObject> evidenceList = new List<GameObject>();
+    
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void ViewList()
+    {
+        if(Input.GetKey(KeyCode.E))
+        {
+            gameObject.GetComponent<CanvasGroup>().alpha = 1;
+        }
+        else
+        {
+            gameObject.GetComponent<CanvasGroup>().alpha = 0;
+        }
+    }
+
+    void GetEvidence()
+    {
+        evidenceList = ResourceManager.evidenceList;
+    }
+
+    void FillList()
+    {
+        for(int i = 0; i < evidenceList.Count; i++)
+        {
+            GameObject textElement = Instantiate(Resources.Load("/Prefabs/EvidenceElement")) as GameObject;
+            textElement.name = "EvidenceText " + i;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ViewList();
+        GetEvidence();
+    }
+}
