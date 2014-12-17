634e981ca7794c6e44a2a2739f790774ee316d33
Rense <rensinjo@hotmail.com>
2014-12-17 17:37:33 +0100

    test met data opslaan over scenes

---
+++ Unity/Assets/Scripts/GotoNextScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GotoNextScene : MonoBehaviour {
+	public GameObject Buttons;
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.tag=="Player")
+			Buttons.SetActive (true);
+	}
+
+	void OnTriggerExit(Collider other){
+		if(other.gameObject.tag=="Player")
+			Buttons.SetActive (false);
+	}
+}
