ed55bf4e4ee3a14dc0d7fd40775fabe94e54b54f
Jesper Spillenaar Bilgen <jesper_sb_91@hotmail.com>
2014-12-19 17:32:23 +0100

    street lights horizontal streets placed + update how to play

--- Unity/Assets/Scripts/landpost.cs
+++ Unity/Assets/Scripts/landpost.cs
@@ -3,9 +3,44 @@ using System.Collections;
 
 public class landpost : MonoBehaviour {
 
+	public Transform light;
+
 	// Use this for initialization
 	void Start () {
-		Object instance = Instantiate(Resources.Load("prefabs/street light", typeof(Object)));
+		for (int x = 4; x < 236; x = x + 20){
+			Instantiate(light, new Vector3 (x, 0, -199), Quaternion.Euler(270,0,0));
+			Instantiate(light, new Vector3 (x, 0, -191), Quaternion.Euler(270,0,0));
+		}
+
+		for (int x = -176; x < 236; x = x + 20){
+			Instantiate(light, new Vector3 (x, 0, -146), Quaternion.Euler(270,0,0));
+			Instantiate(light, new Vector3 (x, 0, -154), Quaternion.Euler(270,0,0));
+		}
+
+		for (int x = -256; x < 236; x = x + 20){
+			Instantiate(light, new Vector3 (x, 0, -94), Quaternion.Euler(270,0,0));
+			Instantiate(light, new Vector3 (x, 0, -86), Quaternion.Euler(270,0,0));
+		}
+
+		for (int x = -256; x < -66; x = x + 20){
+			Instantiate(light, new Vector3 (x, 0, -4), Quaternion.Euler(270,0,0));
+			Instantiate(light, new Vector3 (x, 0, 4), Quaternion.Euler(270,0,0));
+		}
+
+		for (int x = 190; x < 324; x = x + 20){
+			Instantiate(light, new Vector3 (x, 0, -4), Quaternion.Euler(270,0,0));
+			Instantiate(light, new Vector3 (x, 0, 4), Quaternion.Euler(270,0,0));
+		}
+
+		for (int x = -176; x < 176; x = x + 20){
+			Instantiate(light, new Vector3 (x, 0, 86), Quaternion.Euler(270,0,0));
+			Instantiate(light, new Vector3 (x, 0, 94), Quaternion.Euler(270,0,0));
+		}
+
+		for (int x = -216; x < 281; x = x + 20){
+			Instantiate(light, new Vector3 (x, 0, 146), Quaternion.Euler(270,0,0));
+			Instantiate(light, new Vector3 (x, 0, 154), Quaternion.Euler(270,0,0));
+		}
 	}
 	
 	// Update is called once per frame
