79aa62951bd8d73a0c2e2eb79606dd6eb449ffe2
Jesper Spillenaar Bilgen <jesper_sb_91@hotmail.com>
2014-12-19 16:29:10 +0100

    update how to play en start land posts plaatsen

---
+++ Unity/Assets/Scripts/landpost.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class landpost : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Object instance = Instantiate(Resources.Load("prefabs/street light", typeof(Object)));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
