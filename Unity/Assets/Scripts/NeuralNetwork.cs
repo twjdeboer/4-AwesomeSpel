f831c148359fec3c9f5250b22784d4deaed8f7ce
Luuk de Niet <l.f.deniet93@gmail.com>
2015-01-06 12:40:32 +0100

    Nog steeds een hoop werk te doen

--- Unity/Assets/Scripts/NeuralNetwork.cs
+++ Unity/Assets/Scripts/NeuralNetwork.cs
@@ -5,30 +5,67 @@ public class NeuralNetwork : MonoBehaviour {
 
 	public int AantalBewijzen;
 	public int AantalUitkomsten; //twee denk ik
+	public int NumHidden;
 
-	System.Random random = new System.Random();
+
 	// Use this for initialization
 	void Start () {
 	
 		// Maak het neural network
-		double[,] Weigths = new double[AantalBewijzen, AantalUitkomsten];
+		double[,] W1 = new double[AantalBewijzen, NumHidden];
+		double[,] W2 = new double[NumHidden, AantalUitkomsten];
+		double[] T1 = new double[NumHidden];
+		double[] T2 = new double[AantalUitkomsten];
+
 		// Insert random weigths
 		for (int i = 0; i < AantalBewijzen; i++) 
 		{
+			for (int j = 0; j < NumHidden; j++) 
+			{
+				double blabla = Random.Range (0.0f, 1.0f);
+				W1 [i, j] = (blabla * AantalBewijzen) - (AantalBewijzen / 2);
+			}
+		}
+		for (int i = 0; i < NumHidden; i++) 
+		{
 			for (int j = 0; j < AantalUitkomsten; j++) 
 			{
-				double blabla = Random.Range (0,100);
-				blabla = blabla / 100;
-				Weigths [i, j] = (blabla * AantalBewijzen) - (AantalBewijzen / 2);
+				double blabla = Random.Range (0.0f, 1.0f);
+				W2 [i, j] = (blabla * NumHidden) - (NumHidden / 2);
+			}
+		}
+		for (int i = 0; i < NumHidden; i++) 
+		{
+			double blabla = Random.Range (0.0f, 1.0f);
+			T1[i] = (blabla * NumHidden) - (NumHidden / 2);
+		}
+
+		for (int i = 0; i < AantalUitkomsten; i++) 
+		{
+			double blabla = Random.Range (0.0f, 1.0f);
+			T2[i] = (blabla * AantalUitkomsten) - (AantalUitkomsten / 2);
+		}
+		//define a random testset
+		int[,] TestSet = new int[50, AantalBewijzen + 1];
+
+		for (int i = 0; i<50; i++) 
+		{
+			for (int j=0; j<AantalBewijzen+1; j++) 
+			{
+				if (j=AantalBewijzen)
+				{
+					TestSet[i, j]= Random.Range(0, AantalUitkomsten);
+				}
+				else
+				{
+					TestSet [i, j] = Random.Range (0, 2);
+				}
+
 			}
 		}
 
-		//train the weigths
-		// first we make a random trainingset
-		double[,] TestSet = new double[50, AantalBewijzen + 1];
-		for (int i=0;i<50;++i)
-			for (int j=0;j<AantalBewijzen+1;++j)
-				TestSet[i, j] = random.NextDouble();
+
+
 
 	}
 	
@@ -38,11 +75,7 @@ public class NeuralNetwork : MonoBehaviour {
 	}
 
 
-
-
-
-
 }
 
 
-
+	
