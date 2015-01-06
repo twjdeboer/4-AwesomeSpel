a208c20dc655a86d1626d08d1e3241000b493f38
Jesper Spillenaar Bilgen <jesper_sb_91@hotmail.com>
2015-01-06 09:50:13 +0100

    lantaarnpalen script af

--- Unity/Assets/Scripts/landpost.cs
+++ Unity/Assets/Scripts/landpost.cs
@@ -7,6 +7,8 @@ public class landpost : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+		//horizontal landposts
 		for (int x = 4; x < 236; x = x + 20){
 			Instantiate(light, new Vector3 (x, 0, -199), Quaternion.Euler(270,0,0));
 			Instantiate(light, new Vector3 (x, 0, -191), Quaternion.Euler(270,0,0));
@@ -40,6 +42,75 @@ public class landpost : MonoBehaviour {
 		for (int x = -216; x < 281; x = x + 20){
 			Instantiate(light, new Vector3 (x, 0, 146), Quaternion.Euler(270,0,0));
 			Instantiate(light, new Vector3 (x, 0, 154), Quaternion.Euler(270,0,0));
+		}
+
+		//vertical landposts
+
+		Instantiate(light, new Vector3 (-4, 0, -172), Quaternion.Euler(270,0,0));
+		Instantiate(light, new Vector3 (4, 0, -172), Quaternion.Euler(270,0,0));
+			
+		Instantiate(light, new Vector3 (236, 0, -172), Quaternion.Euler(270,0,0));
+		Instantiate(light, new Vector3 (244, 0, -172), Quaternion.Euler(270,0,0));
+
+		for (int z = 24; z < 130; z = z + 20){
+			Instantiate(light, new Vector3 (-229, 0, z), Quaternion.Euler(270,0,0));
+			Instantiate(light, new Vector3 (-221, 0, z), Quaternion.Euler(270,0,0));
+
+			Instantiate(light, new Vector3 (236, 0, z), Quaternion.Euler(270,0,0));
+			Instantiate(light, new Vector3 (244, 0, z), Quaternion.Euler(270,0,0));
+
+			Instantiate(light, new Vector3 (281, 0, z), Quaternion.Euler(270,0,0));
+			Instantiate(light, new Vector3 (289, 0, z), Quaternion.Euler(270,0,0));
+		}
+
+		for (int z = -132; z < -100; z = z + 20){
+			Instantiate(light, new Vector3 (-184, 0, z), Quaternion.Euler(270,0,0));
+			Instantiate(light, new Vector3 (-176, 0, z), Quaternion.Euler(270,0,0));
+
+			Instantiate(light, new Vector3 (-4, 0, z), Quaternion.Euler(270,0,0));
+			Instantiate(light, new Vector3 (4, 0, z), Quaternion.Euler(270,0,0));
+
+			Instantiate(light, new Vector3 (86, 0, z), Quaternion.Euler(270,0,0));
+			Instantiate(light, new Vector3 (94, 0, z), Quaternion.Euler(270,0,0));
+
+			Instantiate(light, new Vector3 (236, 0, z), Quaternion.Euler(270,0,0));
+			Instantiate(light, new Vector3 (244, 0, z), Quaternion.Euler(270,0,0));
+		}
+
+		for (int z = -65; z < -20; z = z + 20){
+			Instantiate(light, new Vector3 (-184, 0, z), Quaternion.Euler(270,0,0));
+			Instantiate(light, new Vector3 (-176, 0, z), Quaternion.Euler(270,0,0));
+			
+			Instantiate(light, new Vector3 (-124, 0, z), Quaternion.Euler(270,0,0));
+			Instantiate(light, new Vector3 (-116, 0, z), Quaternion.Euler(270,0,0));
+			
+			Instantiate(light, new Vector3 (176, 0, z), Quaternion.Euler(270,0,0));
+			Instantiate(light, new Vector3 (184, 0, z), Quaternion.Euler(270,0,0));
+			
+			Instantiate(light, new Vector3 (236, 0, z), Quaternion.Euler(270,0,0));
+			Instantiate(light, new Vector3 (244, 0, z), Quaternion.Euler(270,0,0));
+		}
+
+		for (int z = 25; z < 70; z = z + 20){
+			Instantiate(light, new Vector3 (-184, 0, z), Quaternion.Euler(270,0,0));
+			Instantiate(light, new Vector3 (-176, 0, z), Quaternion.Euler(270,0,0));
+			
+			Instantiate(light, new Vector3 (-124, 0, z), Quaternion.Euler(270,0,0));
+			Instantiate(light, new Vector3 (-116, 0, z), Quaternion.Euler(270,0,0));
+			
+			Instantiate(light, new Vector3 (176, 0, z), Quaternion.Euler(270,0,0));
+			Instantiate(light, new Vector3 (184, 0, z), Quaternion.Euler(270,0,0));
+		}
+
+		for (int z = 110; z < 140; z = z + 20){
+			Instantiate(light, new Vector3 (-184, 0, z), Quaternion.Euler(270,0,0));
+			Instantiate(light, new Vector3 (-176, 0, z), Quaternion.Euler(270,0,0));
+			
+			Instantiate(light, new Vector3 (-4, 0, z), Quaternion.Euler(270,0,0));
+			Instantiate(light, new Vector3 (4, 0, z), Quaternion.Euler(270,0,0));
+			
+			Instantiate(light, new Vector3 (146, 0, z), Quaternion.Euler(270,0,0));
+			Instantiate(light, new Vector3 (154, 0, z), Quaternion.Euler(270,0,0));
 		}
 	}
 	
