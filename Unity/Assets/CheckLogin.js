a499a96039de75790ed48910ce34ea1cbde47eca
Thomas de Boer <twjdeboer+github@gmail.com>
2015-01-07 15:20:23 +0100

    Started work on Clientside Javascript.

--- Unity/Assets/CheckLogin.js
+++ Unity/Assets/CheckLogin.js
@@ -11,4 +11,11 @@ function Update () {
 function CheckLogin() {
 		var username = GameObject.Find ("username").GetComponent<InputField>().text;
 		var password = GameObject.Find ("password").GetComponent<InputField>().text;
+		
+		var req = {user: username, pass: password};
+		
+		var xmlHttp = null;
+		
+		xmlHttp = new XMLHttpRequest();
+		xmlHttp.open( "GET", 
 }
