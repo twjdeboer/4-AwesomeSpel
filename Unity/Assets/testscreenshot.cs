1294a529cbda8424765c9baa38de3b1a63358b49
Rense <rensinjo@hotmail.com>
2015-01-16 17:00:03 +0100

    materials goeie shader + test nieuwe minimap

---
+++ Unity/Assets/testscreenshot.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testscreenshot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("p")) {
+			Application.CaptureScreenshot("SCREENSHOTTEST.png");
+		}
+
+	}
+}
