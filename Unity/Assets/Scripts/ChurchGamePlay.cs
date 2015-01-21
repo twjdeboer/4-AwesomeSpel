d4482a5bdd9a873856cf024afba61451f3662872
Luuk de Niet <l.f.deniet93@gmail.com>
2015-01-21 15:14:26 +0100

    The server resets your evidence when you finish the game

--- Unity/Assets/Scripts/ChurchGamePlay.cs
+++ Unity/Assets/Scripts/ChurchGamePlay.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Text.RegularExpressions;
 using System.IO;
+using SimpleJSON;
 
 public class ChurchGamePlay : MonoBehaviour {
 
@@ -48,8 +49,28 @@ public class ChurchGamePlay : MonoBehaviour {
 
 
 			if (npcinteraction.textToDisplay == "Oh no"){
-			Application.LoadLevel("Einde");
+
+				string filename = "cloud.save";
+				string[] content = File.ReadAllLines (filename);
+
+				string resulturl = "http://drproject.twi.tudelft.nl:8084/resetSave?userId=" + content [0];
+				WWW resultget = new WWW (resulturl);
+				StartCoroutine (GETAddEvidence (resultget));
+
+
+				Application.LoadLevel("Einde");
 				}
 
+	}
+
+	// method for sending a get request
+	IEnumerator GETAddEvidence(WWW www){
+		yield return www;
+
+		if (www.error == null) {
+			Debug.Log ("SUCCESS");
+				} else {
+			Debug.Log ("WWW Error:" + www.error	);
+		}
 	}
 }
